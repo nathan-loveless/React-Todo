ebbc13d7d0d27dc7210e49fbe8275537584a1f33
Nathan S. Loveless <nloveles@norwich.edu>
2019-10-31 15:46:40 -0400

    Uplading current MVP work.

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,21 @@
+// your components will all go in this `component` directory.
+// feel free to change this component.js into TodoList.js
+import React from 'react';
+
+class Todo extends React.Component {
+  // you will need a place to store your state in this component.
+  // design `App` to be the parent component of your application.
+  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return (
+      <div>{this.props.item}</div>
+    );
+  }
+}
+
+export default Todo;
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,34 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Todo from './Todo';
+
+class TodoList extends React.Component {
+  // you will need a place to store your state in this component.
+  // design `App` to be the parent component of your application.
+  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(props) {
+    super(props);
+    this.state = {
+        list: props.list
+      }
+
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.list.map(item => {
+          return (
+            <div>
+              <Todo item={item} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default TodoList;
+
